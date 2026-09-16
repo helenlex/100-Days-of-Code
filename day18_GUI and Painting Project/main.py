52b113e18a6ddb83e31c52a379ac03f535dee2d2
helenlex <helenleother@gmail.com>
2026-09-17 00:01:10 +1000

    challenge 4 complete

--- day18_GUI and Painting Project/main.py
+++ day18_GUI and Painting Project/main.py
@@ -1,5 +1,7 @@
-import turtle as t # using an alias
+""" doing challenges and playing around with turtle module """
+
 import random as r
+import turtle as t  # using an alias
 
 monty = t.Turtle()
 monty.shape("turtle")
@@ -28,9 +30,25 @@ for x in range (3, 11):
         monty.forward(100)
         angle += 360 / x
 
+
+# challenge 4 - random walk
+    # get turtle to randomly walk (turn in different directions during its walk)
+    # line should be thicker
+    # each line should be a random colour
+
+while True:
+    monty.width(10)
+    to_angle = [0, 90, 180, 270]
+    angle = r.choice(to_angle)
+    monty.pencolor(r.random(),r.random(),r.random())
+    monty.seth(angle)
+    monty.speed(10)
+    monty.forward(30)
+
 # print to screen
-screen = t.Screen()
-screen.exitonclick()
+SCREEN = t.Screen()
+SCREEN.canvheight(50)
+SCREEN.canvwidth(50)
+SCREEN.exitonclick()
 
 # remember, modules get installed in your project only, (local environment only)
-
