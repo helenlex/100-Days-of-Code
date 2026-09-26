60820662ad63f9e5c172da72efeabd6f111356f5
helenlex <helenleother@gmail.com>
2026-09-26 20:43:19 +1000

    FINISHED THE PONG GAME FINALLY

--- day22_Pong Game Project/main.py
+++ day22_Pong Game Project/main.py
@@ -1,10 +1,9 @@
+import time
 from turtle import Screen
 
-from paddle import R_Paddle, L_Paddle
-
 from ball import Ball
-
-import time
+from paddle import L_Paddle, R_Paddle
+from scoreboard import Scoreboard
 
 # CREATE THE SCREEN
 SCREEN = Screen()
@@ -28,22 +27,38 @@ SCREEN.listen()
 SCREEN.onkeypress(fun = l_paddle.move_up, key= "w")
 SCREEN.onkeypress(fun = l_paddle.move_down, key= "s")
 
+# CREATE A SCOREBOARD
+scoreboard = Scoreboard()
+
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.move_speed)
     SCREEN.update()
-    time.sleep(0.1)
     ball.move()
 # DETECT COLLISION WITH WALL AND BOUNCE
     # COLLISION ON TOP AND BOTTOM WALLS ONLY, AND BOUNCE
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # DETECT COLLISION WITH R PADDLE
+    # DETECT COLLISION WITH PADDLE
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-# DETECT WHEN PADDLE MISSES
+    
+    # DETECT WHEN BALL GOES OUT OF BOUNDS AT EDGE OF THE SCREEN
+    # IF OUT OF BOUNDS, BRING BALL BACK TO CENTER OF SCREEN
+    # BALL SHOULD MOVE TOWARDS OTHER PLAYER
+
+# DETECT WHEN R PADDLE MISSES
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+# # DETECT WHEN L PADDLE MISSES
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 # KEEP SCORE
     # IF LEFT AND RIGHT WALLS ARE HIT, POINT TO THE OPPONENT
